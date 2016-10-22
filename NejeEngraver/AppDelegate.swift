79cd58dfadfd87807e7f2a009b09c9e05f4e47e3
AndyQ <andy.qua@gmail.com>
2016-10-22 17:47:45 +0100

    Initial Commit

---
+++ NejeEngraver/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NejeEngraver
+//
+//  Created by Andy Qua on 22/10/2016.
+//  Copyright © 2016 Andy Qua. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NejeEngraver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NejeEngraver
+//
+//  Created by Andy Qua on 22/10/2016.
+//  Copyright © 2016 Andy Qua. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
